ea14ce77f89cfef7302a540abc561a2c3f84ab1c
jeremy dahan <jeremydahan@live.fr>
2020-08-28 12:11:31 +0200

    python

---
+++ parite.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+pour charger l'environement env il faut taper la commande suivante:
+.\/env/Scripts/activate.ps1
+
+pour le desactiver il suffit de taper :
+deactivate
+
+pour supprimer un repertoire Remove-Item "nom du repertoire" -force
+
+"""
+
+def main() :
+    pass
+
+if __name__ == "main" :
+    main()
+
